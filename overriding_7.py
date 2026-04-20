2989b77770c1f77552b1ba0c504039107ec40278
yashtribhuvan777-prog <yashtribhuvan777@gmail.com>
2026-04-21 00:28:21 +0530

    Add files via upload

---
+++ overriding_7.py
@@ -0,0 +1,20 @@
+# Main class
+class Vehicle:
+	def move(self):
+		print("Vehicle is moving")
+
+# Subclass Car
+class Car(Vehicle):
+	def move(self):
+		print("Driving on the road")
+
+# Subclass Bicycle
+class Bicycle(Vehicle):
+	def move(self):
+		print("Pedaling on the road")
+
+# Showing polymorphism
+vehicles = [Car(), Bicycle()]
+
+for v in vehicles:
+	v.move()
