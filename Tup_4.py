df0816ff6a765e0da7e74bae9f103af801a6bf31
yashtribhuvan777-prog <yashtribhuvan777@gmail.com>
2026-04-21 00:10:37 +0530

    Add files via upload

---
+++ Tup_4.py
@@ -0,0 +1,24 @@
+tup1=(10,20,30,40,50,60)
+print(tup1)
+
+#print any element print 
+print (tup1 [0],)
+
+#create second tuple
+tup2 = (1,2,3,4,5,6,)
+print(tup2)
+
+#print any element
+print (tup2[-1],)
+
+#concatenation both tuple
+concatenated_tuple=tup1+tup2
+print(concatenated_tuple)
+
+#repetition of tuple
+repeated_tuple=tup2*3
+print(repeated_tuple)
+
+#nested tuple
+nested_tuple=(1,2,3,(10,20,30,40,50,60),4,5,6)
+print(nested_tuple)
